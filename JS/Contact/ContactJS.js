4a83b567058aca66a9325aa86a1357a1db15d0b2
Zyad Khalef <118022693+ZyadKH@users.noreply.github.com>
2023-07-21 23:19:14 +0200

    Adjusted the recrutement, and problem encoutered with contact page, no links of social media work

--- JS/Contact/ContactJS.js
+++ JS/Contact/ContactJS.js
@@ -4,3 +4,20 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
   e.target.elements.email.value = '';
   e.target.elements.message.value = '';
 });
+
+// Social media icons event listeners
+document.getElementById('github-icon').addEventListener('click', function() {
+  window.open('https://www.linkedin.com/company/emientreprises/mycompany/', '_blank');
+});
+
+document.getElementById('codepen-icon').addEventListener('click', function() {
+  window.open('https://www.facebook.com/emientreprises', '_blank');
+});
+
+document.getElementById('twitter-icon').addEventListener('click', function() {
+  window.open('https://www.instagram.com/forumemientreprises/', '_blank');
+});
+
+document.getElementById('instagram-icon').addEventListener('click', function() {
+  window.open('https://www.youtube.com/@ForumEMIEntreprises22/videos', '_blank');
+});
