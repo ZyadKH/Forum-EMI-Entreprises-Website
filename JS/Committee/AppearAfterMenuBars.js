2707d6e19e2dac5c601e3a031a4630974998a869
Zyad Khalef <118022693+ZyadKH@users.noreply.github.com>
2023-07-30 17:50:07 +0200

    Comité soutien

--- JS/Committee/AppearAfterMenuBars.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-  // Get the trigger button and committee section elements
-  const triggerButton = document.querySelector('.menu-trigger');
-  const committeeSection = document.querySelector('.committee-section');
-
-  // Insert the committee section element after the trigger button as a sibling
-  triggerButton.insertAdjacentElement('afterend', committeeSection);
-});
