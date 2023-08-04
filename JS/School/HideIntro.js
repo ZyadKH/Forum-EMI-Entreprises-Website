8c66876169bff59684c354cae9bad04804fbfb27
Zyad Khalef <118022693+ZyadKH@users.noreply.github.com>
2023-08-04 16:00:04 +0200

    small

--- JS/School/HideIntro.js
+++
@@ -1,8 +0,0 @@
-function toggleIntroduction() {
-    const introduction = document.getElementById("introduction");
-    if (introduction.style.display === "none") {
-        introduction.style.display = "block";
-    } else {
-        introduction.style.display = "none";
-    }
-}
