017e2f728abeb532d17f4a655e10be3c76bc9961
Zyad Khalef <118022693+ZyadKH@users.noreply.github.com>
2023-08-31 18:55:14 +0200

    Backend

--- server.js
+++ server.js
@@ -1,47 +1,50 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-require('dotenv').config(); // Load environment variables
+require('dotenv').config();
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const port = process.env.PORT || 3000;
 
-// Nodemailer configuration
-const transporter = nodemailer.createTransport({
-    host: "node36-eu.n0c.com", // Update the server hostname
-    port: 465, // Update to the outgoing SMTP port you want to use (465 or 587)
-    secure: true, // Set to true because you're using port 465 (secure connection)
-    auth: {
-        user: process.env.EMAIL_USER, // Use environment variable for email
-        pass: process.env.EMAIL_PASS // Use environment variable for password
-    }
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public_html'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public_html/contact.html');
 });
 
-// Route to handle form submission
-app.post('/server', (req, res) => {
-    console.log(req)
+
+app.post('/send', (req, res) => {
     const { name, email, message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+        host: 'node36-eu.n0c.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+    
 
     const mailOptions = {
         from: email,
         to: 'contact@forumemientreprises.com',
-        subject: 'Contact Par SiteWeb',
-        text: `De la part de: ${name}, \nMessage: ${message}`
+        subject: 'New Contact Form Submission',
+        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             console.log(error);
-            res.status(500).send('Error sending email');
+            res.send('Error');
         } else {
             console.log('Email sent: ' + info.response);
-            res.send('Email sent successfully');
+            res.send('Success');
         }
     });
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
